8bd8f98b76765539859f7ce259a75020ffd57721
Omar Stefan Evans <omar@evansbros.info>
2014-12-16 14:10:02 -0500

    Added iOS Projects (DampKeg Client & Rambler Framework)

---
+++ DampKeg/Apple/OS X/Source Code/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DampKeg
+//
+//  Created by Omar Evans on 2014/12/16.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DampKeg/Apple/iOS/Source Code/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DampKeg (iOS)
+//
+//  Created by Omar Evans on 2014/12/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
